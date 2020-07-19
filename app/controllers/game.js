f6ce73a3ab819a3e0cb8b9b8bafb0b929c6ce097
StanislavKosyy <stanislav.kosyy@mail.ru>
2020-07-19 22:07:17 +0300

    компания

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -1,3 +1,10 @@
-exports.new = function(request, response){
-	response.render("game/new");
+const fs = require("fs");
+exports.newCampany = function(request, response){
+	var campany = JSON.parse(fs.readFileSync("app/campanies/dungeon_of_the_black_castle.json", 'utf8'));
+	var id = request.params.id;
+	campany.forEach((location) => {
+		if(location.id = id){
+			response.render("game/location", {"location": location});
+		}
+	});
 };
